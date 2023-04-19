3169a39dbc6e74828485c97e715ac3edf9461c1c
DevMaximiliano <mafarias@andreani.com>
2023-04-19 14:07:13 -0300

    libreria RabbitMQBus

---
+++ RabbitMq.Bus/BusRabbit/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMq.Bus.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMq.Bus.BusRabbit
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ RabbitMq.Bus/BusRabbit/IRabbitEventBus.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMq.Bus.Commands;
+using RabbitMq.Bus.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMq.Bus.BusRabbit
+{
+    public interface IRabbitEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T evento) where T : Event;
+        void Suscribe<T, TH>() where T : Event
+                                where TH : IEventHandler<T>;
+    }
+}
